e962d832cc2ffb430fdbcdded381f3c3023deae9
Gerardo2000-ajax <85649446+Gerardo2000-ajax@users.noreply.github.com>
2022-11-30 21:59:15 -0600

    Add files via upload

---
+++ usuarioRuta.js
@@ -0,0 +1,16 @@
+'use strict'
+
+var express = require('express');
+var UsuarioControl = require('../controlador/usuarioControl');
+const usuarios = require('../modelo/usuarios');
+
+var api = express.Router();
+
+api.get('/probando-controlador', UsuarioControl.prueba);
+
+api.post('/registro', UsuarioControl.registrarUsuario);
+
+api.post('/login',UsuarioControl.accesoUsuario);
+
+module.exports = api;
+
